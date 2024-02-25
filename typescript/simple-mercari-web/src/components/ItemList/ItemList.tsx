5c65fa599b3974161cf50b9700624fb1e5629f1f
holarora <liu.lijia.82v@st.kyoto-u.ac.jp>
2024-02-25 15:36:21 +0900

    replace the placeholder image

--- typescript/simple-mercari-web/src/components/ItemList/ItemList.tsx
+++ typescript/simple-mercari-web/src/components/ItemList/ItemList.tsx
@@ -51,7 +51,9 @@ export const ItemList: React.FC<Prop> = (props) => {
         return (
           <div key={item.id} className='ItemList'>
             {/* TODO: Task 1: Replace the placeholder image with the item image */}
-            <img src={placeholderImage} />
+            <img
+              src={"http://localhost:9000/image/" + item.image_name}
+              alt={placeholderImage} />
             <p>
               <span>Name: {item.name}</span>
               <br />
