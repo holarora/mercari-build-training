f3f003fc86e93aea1f7a474a42fa1263c2aed184
holarora <liu.lijia.82v@st.kyoto-u.ac.jp>
2024-02-29 14:37:51 +0900

    intersection of two linked lists

--- extra/extra-01/numbersDisappeared.py
+++ extra/extra-01/numbersDisappeared.py
@@ -17,6 +17,7 @@ class Solution(object):
             if i not in nums:
                 numsMissing.append(i)
         return numsMissing
+
 if __name__ == "__main__":
     nums = [4,3,2,7,8,2,3,1]
     result = Solution().numbersDisappeared(nums)
